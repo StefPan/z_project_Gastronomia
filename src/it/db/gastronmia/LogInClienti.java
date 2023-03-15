72eb1efffc3ae6357f48b04a9e981bb87376054e
StefPan <127962928+StefPan@users.noreply.github.com>
2023-03-15 17:47:12 +0100

    15_03_23 Changes

--- src/it/db/gastronmia/LogInClienti.java
+++ src/it/db/gastronmia/LogInClienti.java
@@ -25,22 +25,36 @@ public class LogInClienti {
 		 String address;
 		 Integer cap;
 		 String city;
+		 
+		 String userEmail;
+		 String password;
+		 String answer;
+		 
 		Connection connection = null;
-		String userEmail;
 		System.out.println("Inserisci il tuo indirizzo email:");
 		userEmail = scanner.nextLine();
 
 		try {
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
 			Statement statement = connection.createStatement();
-			ResultSet leggiTab;
-			leggiTab=statement.executeQuery("SELECT * FROM clienti  WHERE email =" + userEmail);
-			
-			if(leggiTab != null) {}
-			if(leggiTab ==null) {
+			ResultSet leggiTab1;
+			ResultSet leggiTab2;
+			leggiTab1=statement.executeQuery("SELECT * FROM clienti  WHERE email =" + userEmail);
+		//cambia questi leggitab con leggiTab.get...qualcosa...fai check...che non sono stringhe	
+			if(leggiTab1.get != null) {
+				System.out.println("Inserisci Password:");
+				password= scanner.nextLine();
+				leggiTab2=statement.executeQuery("SELECT paswrd FROM  clienti WHERE email = " + userEmail + ";");
+				if(password.equals(leggiTab2)) {
+					System.out.println("Il tuo indirizzo è corretto? si/no");
+					answer= scanner.nextLine();
+					//fai do while per controllo risposta e in caso immetti nuovo indirizzo con update modify...
+				}
+			}
+			if(leggiTab1.getString("passwrd") ==null) {
 				System.out.println("Utente non registrato, procedere alla registrazione.\n\nInserisci la tua email: ");
-				
-				//RIPORVIAMO!!!!
+			//porsegui a fare la registrazione  con scanner.next e poi fai insert into clienti ed inserisci..	
+			
 			}
 			
 		} catch (SQLException e) {
---
+++ src/it/db/gastronmia/Singleton.java
@@ -0,0 +1,30 @@
+package it.db.gastronmia;
+
+public class Singleton {
+
+	private String value;
+
+	
+	 public static Singleton instance= null;
+	 
+	 private Singleton() {}
+	 
+	 public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+	public static Singleton getInstance() {
+		if(instance==null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+	
+
+	
+}
